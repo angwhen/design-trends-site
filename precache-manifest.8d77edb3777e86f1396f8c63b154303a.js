f72f703c809dfdbec599bd6a8bf8ca7105022fce
angwhen <angelmwen@gmail.com>
2019-09-24 16:19:46 -0400

    Updates

---
+++ precache-manifest.8d77edb3777e86f1396f8c63b154303a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f07c7821b5beeb2601407e668ccbf7",
+    "url": "/design-trends-site/index.html"
+  },
+  {
+    "revision": "5230d3f64ec6f5b194c8",
+    "url": "/design-trends-site/static/css/2.fdd1b033.chunk.css"
+  },
+  {
+    "revision": "5230d3f64ec6f5b194c8",
+    "url": "/design-trends-site/static/js/2.d0a75c69.chunk.js"
+  },
+  {
+    "revision": "22f981e869a5c6bba9b0",
+    "url": "/design-trends-site/static/js/main.68b3c4a3.chunk.js"
+  },
+  {
+    "revision": "23a77ca2552fa684fbb5",
+    "url": "/design-trends-site/static/js/runtime~main.62919d4f.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/design-trends-site/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/design-trends-site/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/design-trends-site/static/media/revicons.2feb69cc.eot"
+  }
+]);
