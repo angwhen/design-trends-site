563c8187e20d092fec84e83ee45c27778b84d034
angwhen <angelmwen@gmail.com>
2019-11-24 18:58:00 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/design-trends-site/precache-manifest.30047e906bf99f2a1b0f3e8c90476c01.js"
+  "/design-trends-site/precache-manifest.a236a625dfe95a37dfa5ab685487464c.js"
 );
 
 self.addEventListener('message', (event) => {
